044e3aab8d3d73b73c367be19a3d059c42dbb091
padowla <62257411+padowla@users.noreply.github.com>
2023-01-23 18:02:46 +0100

    Add files via upload

---
+++ Transfer-Group-Membership-Active-Directory/export-users-from-groups.ps1
@@ -0,0 +1,14 @@
+﻿#Import active directory module for running AD cmdlets
+
+Import-Module activedirectory
+$count = 0
+
+$GroupsFromDomain = Import-csv "C:\Users\adm.samuelep\Downloads\Migrate-Group-Membership-AD\groups-from-domain.csv" -Delimiter ";"
+
+$GroupsFromDomain | Format-Table
+
+foreach ($Group in $GroupsFromDomain) {
+    Write-Output "Total number of members of group $($Group.Groups): $((Get-ADGroup $Group.Groups -Properties *).Member.Count) "
+    #Write-Output "Members of group: $($Group.Groups)"
+    Get-ADGroupMember -Identity $Group.Groups | select SamAccountName | Export-CSV -Path "C:\Users\adm.samuelep\Downloads\Migrate-Group-Membership-AD\$($Group.Groups).csv" -Notypeinformation
+}
---
+++ Transfer-Group-Membership-Active-Directory/import-users-memberships.ps1
@@ -0,0 +1,25 @@
+﻿#Import active directory module for running AD cmdlets
+
+Import-Module activedirectory
+$count = 0
+
+$GroupsFromDomain = Import-csv "C:\Users\Administrator\Downloads\Migrate-Group-Membership-AD\groups-from-domain.csv" -Delimiter ";"
+
+$GroupsFromDomain | Format-Table
+
+foreach ($Group in $GroupsFromDomain) {
+    Write-Output "--------------------------------------------------------------------------------------------"
+    Write-Output "Membership transfer for the following group: $($Group.Groups)"
+    #import previous exported csv from domain of origin
+    $GroupCSV = Import-csv "C:\Users\Administrator\Downloads\Migrate-Group-Membership-AD\$($Group.Groups).csv" -Delimiter ";"
+    #$GroupCSV | Format-Table
+    foreach($User in $GroupCSV){
+        $ret = Add-ADGroupMember -Identity $Group.Groups -Members $($User.SamAccountName) -PassThru
+        #$ret is equal to group if fail to find/add member 
+        Write-Output "Member $($User.SamAccountName) added to $($Group.Groups)"
+        $count++
+      
+    }
+}
+
+Write-Output "Succesfully transfered $count accounts"
