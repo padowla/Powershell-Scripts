cbc9e573159095fd267a31f2fe7c980b149ce5d1
padowla <62257411+padowla@users.noreply.github.com>
2023-01-23 18:09:11 +0100

    Update export-users-from-groups.ps1

--- Transfer-Group-Membership-Active-Directory/export-users-from-groups.ps1
+++ Transfer-Group-Membership-Active-Directory/export-users-from-groups.ps1
@@ -3,12 +3,12 @@
 Import-Module activedirectory
 $count = 0
 
-$GroupsFromDomain = Import-csv "C:\Users\adm.samuelep\Downloads\Migrate-Group-Membership-AD\groups-from-domain.csv" -Delimiter ";"
+$GroupsFromDomain = Import-csv "groups-from-domain.csv" -Delimiter ";"
 
 $GroupsFromDomain | Format-Table
 
 foreach ($Group in $GroupsFromDomain) {
     Write-Output "Total number of members of group $($Group.Groups): $((Get-ADGroup $Group.Groups -Properties *).Member.Count) "
     #Write-Output "Members of group: $($Group.Groups)"
-    Get-ADGroupMember -Identity $Group.Groups | select SamAccountName | Export-CSV -Path "C:\Users\adm.samuelep\Downloads\Migrate-Group-Membership-AD\$($Group.Groups).csv" -Notypeinformation
+    Get-ADGroupMember -Identity $Group.Groups | select SamAccountName | Export-CSV -Path "$($Group.Groups).csv" -Notypeinformation
 }
